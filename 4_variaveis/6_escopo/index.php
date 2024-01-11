efafea1f85fef63eb4fcb9d3d37b35b0bbc6e95a
Thiago Ferreira <thiagocbf@outlook.com>
2024-01-11 15:37:04 -0300

    Escopo Explicações

---
+++ 4_variaveis/6_escopo/index.php
@@ -0,0 +1,8 @@
+<?php
+
+/*
+Global = Fora de funções
+Local = Dentro de uma função
+Static = Dentro da funções , seu valor permanece entre as funções
+Parâmetros de função = Variáveis passadas para uma função, podendo ser utilizadas ao longo da mesma
+*/
