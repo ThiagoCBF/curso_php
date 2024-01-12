01489cdeb297b83d4325abdaaf88547af13c85b4
Thiago Ferreira <thiagocbf@outlook.com>
2024-01-12 16:44:26 -0300

    Operador Módulo

---
+++ 5_operadores_expressoes/07_operador_modulo/index.php
@@ -0,0 +1,14 @@
+<?php
+
+    $num1 = 10;
+    $num2 = 9;
+
+    if($num1 % 2 == 0) {
+        echo "$num1 é par<br>";
+    }
+
+    if($num1 % 9 == 0) {
+        echo "$num1 é par";
+    } else {
+        echo "$num2 é ímpar";
+    }
