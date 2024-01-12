5acb565520b05e93e9ce39f8a9ae0b63109b6a37
Thiago Ferreira <thiagocbf@outlook.com>
2024-01-12 16:38:57 -0300

    Maior e Menor

---
+++ 5_operadores_expressoes/19_maior_menor/index.php
@@ -0,0 +1,18 @@
+<?php
+
+
+    if(5 > 3) {
+        echo "5 é maior que 3<br>";
+    }
+
+    if(5 >= 5) {
+        echo "5 é maior ou igual a 3<br>";
+    }
+
+    if(3 < 5) {
+        echo "3 é menor que 5<br>";
+    }
+
+    if(3 <= 5) {
+        echo "3 é menor que 5<br>";
+    }
