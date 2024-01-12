85f0cfbe308e488dbe313c3572d356c97ae1ee34
Thiago Ferreira <thiagocbf@outlook.com>
2024-01-12 16:38:45 -0300

    Ex16 - Idêntico e Não Idêntico

---
+++ 5_operadores_expressoes/18_ex16/index.php
@@ -0,0 +1,22 @@
+<?php
+
+
+    $num1 = 5;
+    $num2 = 3;
+
+
+    if(5 == 3) {
+        echo "$num1 é igual $num2<br>";
+    }
+
+    if(5 != 3) {
+        echo "$num1 é diferente $num2<br>";
+    }
+
+    if(5 === 3) {
+        echo "$num1 é idêntico a $num2<br>";
+    }
+
+    if(5 !== 3) {
+        echo "$num1 não é idêntico a $num2<br>";
+    }
