55dc63232e4177b001b3e5486361397103d4b821
Thiago Ferreira <thiagocbf@outlook.com>
2024-01-12 16:39:23 -0300

    Ex17 - Operador AND

---
+++ 5_operadores_expressoes/21_ex17/index.php
@@ -0,0 +1,14 @@
+<?php
+
+
+    if(15 > 5 && "João" === "João") {
+        echo "Comparações Verdadeiras 1 <br>";
+    }
+
+    if("teste" > 5 && 1) {  //Comparação entre string e numero = TRUE / 1
+        echo "Comparações Verdadeiras 2 <br>";
+    }
+
+    if(2 == 3 && 5 >= 3) {
+        echo "Comparações Verdadeiras 3 <br>";
+    }
