56f44be92c0ef9414e556050c13092e62b4e6a8c
Thiago Ferreira <thiagocbf@outlook.com>
2024-01-12 16:44:44 -0300

    Operadores Aritméticos

---
+++ 5_operadores_expressoes/05_operadores_aritmeticos/index.php
@@ -0,0 +1,16 @@
+<?php
+
+ $soma = 5 + 5;
+ echo $soma . "<br>";
+
+ $subtracao = 5 - 5;
+ echo $subtracao . "<br>";
+
+ $multi = 5 * 5;
+ echo $soma . "<br>";
+
+ $divisao = 5 / 5;
+ echo $soma . "<br>";
+
+ $op = (5 + 5) / 5 * 5;
+ echo $op . "<br>";
