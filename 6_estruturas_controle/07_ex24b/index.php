823f53400970d2e3558c3c4687dabdf66e29990c
Thiago Ferreira <thiagocbf@outlook.com>
2024-01-15 17:01:53 -0300

    ex24b - If//Else

---
+++ 6_estruturas_controle/07_ex24b/index.php
@@ -0,0 +1,28 @@
+<?php
+
+    $peso1 = 70.24;
+    $peso2 = 90;
+
+    $peso_limite = 80;
+
+    //Var1
+    if($peso1 > $peso_limite) {
+
+        echo "Peso acima do limite<br>";
+
+    } else {
+
+        echo "Peso dentro do limite <br>";
+
+    }
+
+    //Var2
+    if($peso2 > $peso_limite) {
+
+        echo "Peso acima do limite<br>";
+
+    } else {
+
+        echo "Peso dentro do limite <br>";
+
+    }
