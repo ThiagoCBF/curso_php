1030762db20ecbf2650b094db359534f52dabf8b
Thiago Ferreira <thiagocbf@outlook.com>
2024-01-15 17:00:52 -0300

    IF

---
+++ 6_estruturas_controle/01_if/index.php
@@ -0,0 +1,16 @@
+<?php
+
+    //Condição Verdadeira
+    if(5 > 2) {
+
+        echo "Deu certo, Esta no if <br>";
+
+    }
+
+
+    //Condição Falsa
+    if(5 > 2) {
+
+        echo "Deu certo, Esta no if <br>";
+
+    }
