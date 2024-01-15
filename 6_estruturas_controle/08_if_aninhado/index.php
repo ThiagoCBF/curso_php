96a8b845591759c483c0ed2ff2dceb0eeaf09aef
Thiago Ferreira <thiagocbf@outlook.com>
2024-01-15 17:02:03 -0300

    IF aninhado

---
+++ 6_estruturas_controle/08_if_aninhado/index.php
@@ -0,0 +1,25 @@
+<?php
+
+
+if(10 > 2) {
+
+    echo "Entrou no primeiro if <br>";
+
+    if("Teste" == "Teste") {
+
+        echo "Entrou no segundo if <br>";
+    }
+}
+
+if(10 > 2) {
+
+    echo "Entrou no primeiro if <br>";
+
+    if("Teste" == "Testde") {
+
+        echo "Entrou no segundo if <br>";
+    } else {
+
+        echo "Entrou no else 2<br>";
+    }
+}
