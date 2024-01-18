3f368c881674cd6ae1415c6c2cc71db9ddf5d43e
Thiago Ferreira <thiagocbf@outlook.com>
2024-01-18 16:42:06 -0300

    Exibição de Conteúdo

---
+++ 8_inclusao_arquivo/5_exibicao_de_conteudo/index.php
@@ -0,0 +1,19 @@
+<?php
+
+    $nome = "Thiago";
+    $sobrenome = "Ferreira";
+
+?>
+
+<form action="">
+    <div>
+        <input type="text" value="<?= $nome; ?>">
+    </div>
+    <div>
+        <input type="text" value="<?= $sobrenome; ?>">
+    </div>
+    <div>
+        <input type="submit" value="Enviar">
+    </div>
+
+</form>
