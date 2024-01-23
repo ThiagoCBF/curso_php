342bd5e2b170c8cf6744de05f5051878c3974ac4
Thiago Ferreira <thiagocbf@outlook.com>
2024-01-23 16:49:40 -0300

    Auxiliares de Funções

---
+++ 9_funcoes/15_auxiliares_funcoes/index.php
@@ -0,0 +1,15 @@
+<?php
+
+    function soma($a, $b) {
+
+        print_r(func_get_args());
+
+        echo "<br>";
+
+        echo func_num_args() . "<br>";
+
+        return $a + $b;
+
+    }
+
+    soma(2, 4);
