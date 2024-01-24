c8eacb69462029e84e4abb4e3bb8a9796b42fe4a
Thiago Ferreira <thiagocbf@outlook.com>
2024-01-24 19:31:33 -0300

    Printf

---
+++ 10_strings/05_printf/index.php
@@ -0,0 +1,17 @@
+<?php
+
+    $nome = "Thiago";
+
+    // %s -> string
+
+    printf("O nome é %s <br>", $nome);
+
+    // $d -> int
+
+    $x = 10;
+
+    printf("O número é %d<br> ", $x);
+
+    // %f -> float
+
+    printf("A temperatura atual é %.1f <br>", 12.4588);
