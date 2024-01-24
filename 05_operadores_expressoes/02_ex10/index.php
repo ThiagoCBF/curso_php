c7256fbc1abf43d4613b371848f4396f30f972f1
Thiago Ferreira <thiagocbf@outlook.com>
2024-01-24 19:35:12 -0300

    Operadores Expressões

---
+++ 05_operadores_expressoes/02_ex10/index.php
@@ -0,0 +1,11 @@
+<?php
+
+ $num1 = 8;
+ $num2 = 10;
+ $num3 = 2;
+
+ $num4 = ($num1 + $num2) / $num3;
+ echo "($num1 + $num2) / $num3 = $num4<br>";
+
+ $num5 = ($num4 - $num1) * $num3;
+ echo "($num4 - $num1) * $num3 = $num5<br>";
---
+++ 05_operadores_expressoes/04_ex11/index.php
@@ -0,0 +1,14 @@
+<?php
+
+
+ $teste = "5" * 12;
+
+ echo gettype($teste);
+ echo "<br>";
+ echo gettype([]);
+ echo "<br>";
+ echo gettype(12.2);
+ echo "<br>";
+ echo gettype("teste");
+ 
+ 
