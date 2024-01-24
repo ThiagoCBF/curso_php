376e3ced586050e4d6a169075843a4ad6b391356
Thiago Ferreira <thiagocbf@outlook.com>
2024-01-24 19:34:03 -0300

    Create index.php

---
+++ 03_tipos_dados/3_checando_int/index.php
@@ -0,0 +1,16 @@
+<?php
+
+
+ $n = 7;
+
+if (is_int(5)) {
+    echo "É um inteiro<br>";
+}
+
+if (is_int("dsa")) {
+    echo "É um inteiro<br>";
+}
+
+if (is_int($n)) {
+    echo "É um inteiro<br>";
+}
