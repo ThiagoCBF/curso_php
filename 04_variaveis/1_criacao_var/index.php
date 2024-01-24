5ecd5278516fad4cffb8a3d8644b0af5a00af087
Thiago Ferreira <thiagocbf@outlook.com>
2024-01-24 19:34:15 -0300

    Create index.php

---
+++ 04_variaveis/1_criacao_var/index.php
@@ -0,0 +1,20 @@
+<?php
+
+ $teste = "Algum texto";
+
+ echo $teste;
+
+ $num = 1;
+
+ echo "<br>";
+ echo $num;
+
+ $_nome = "Thiago";
+
+ echo "<br>";
+ echo $_nome;
+
+ $velocidade_maxima = 100;
+
+
+
