77adb4b818fa023d44afacac01fad3a7e9b48ce9
Thiago Ferreira <thiagocbf@outlook.com>
2024-01-24 19:31:06 -0300

    Valores de Escape

---
+++ 10_strings/02_valores_escape/index.php
@@ -0,0 +1,17 @@
+<?php
+
+    header("Content-type: text/plain");
+
+    //Pular linha
+    echo "Primeira linha \n Segunda Linha \n";
+
+    //Tab
+    echo "Testando o tab \t aqui \n";
+
+    //Barra Invertida
+    echo "Barra Invertida \\ aqui \n";
+
+    //Dólar
+    echo "Imprimindo o dólar \$teste";
+
+    
