fc943dcf7f09fee233a59285bc0c87e06c3d9a66
Thiago Ferreira <thiagocbf@outlook.com>
2024-01-24 19:34:12 -0300

    Create index.php

---
+++ 03_tipos_dados/9_boolean/index.php
@@ -0,0 +1,19 @@
+<?php
+
+    echo True;
+    echo'<br>';
+    echo False;
+
+    if(true) {
+        echo "É verdadeiro <br>";
+    }
+
+    if(5 > 2) {
+        echo "É verdadeiro <br>";
+    }
+
+    $entrada = True;
+
+    if($entrada) {
+        echo "O usuário pode entrar";
+    }
