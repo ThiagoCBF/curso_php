eab07ec350772e33d6c07d7999f6347f8d69b160
Thiago Ferreira <thiagocbf@outlook.com>
2024-01-26 18:48:52 -0300

    Limpando Espaços

---
+++ 10_strings/09_limpando_string/index.php
@@ -0,0 +1,15 @@
+<?php
+
+    $str1 = "        Thiago";
+
+    echo "Esta é a string 1: $str1. <br>";
+
+    $str1limpa = trim($str1);
+
+    echo "Esta é a string 1: $str1limpa. <br>";
+
+    $str1limpa2 = rtrim($str1);
+
+    echo "Esta é a string 1: $str1limpa2. <br>";
+
+    
