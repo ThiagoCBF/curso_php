5f89cc3c513b194e0774932e204fca652570b1bd
Thiago Ferreira <thiagocbf@outlook.com>
2024-01-30 18:58:33 -0300

    Adicionando No Array

---
+++ 11_arrays/01_adicionando_array/index.php
@@ -0,0 +1,30 @@
+<?php
+
+    $arr = [];
+
+    $arr[0] = 10;
+    $arr[1] = 20;
+    $arr[5] = 60;
+
+
+    print_r($arr);
+    echo "<br>";
+
+    $arr[1] = 70;
+
+    print_r($arr);
+    echo "<br>";
+
+    $arrAssoc = [];
+
+    print_r($arrAssoc);
+    echo "<br>";
+
+    $arrAssoc["Carro"] = "BMW";
+
+    print_r($arrAssoc);
+    echo "<br>";
+
+    $arrAssoc["Carro"] = "Ferrari";
+
+    print_r($arrAssoc);
