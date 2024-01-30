0bc1c22eb7b114af6dd8e98ff19d851a3618f34e
Thiago Ferreira <thiagocbf@outlook.com>
2024-01-30 18:59:04 -0300

    Count Array

---
+++ 11_arrays/05_numero_elementos/index.php
@@ -0,0 +1,13 @@
+<?php
+
+    $arr = [1, 2, 3];
+
+    echo count($arr) . "<br>";
+
+    $arr2 = range(1, 10);
+
+    echo count($arr2) . "<br>";
+
+    $arr3 = ["Nome" => 'Thiago', 'Idade' => 19, 'Nacionalidade' => 'Brasileiro'];
+
+    echo count($arr3) . "<br>";
