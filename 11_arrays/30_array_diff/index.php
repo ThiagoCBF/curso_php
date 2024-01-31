5c256e3c47d49967f9a18c1fbb6b25e87d39c6c9
Thiago Ferreira <thiagocbf@outlook.com>
2024-01-31 20:00:54 -0300

    Array_Diff

---
+++ 11_arrays/30_array_diff/index.php
@@ -0,0 +1,14 @@
+<?php
+
+    $arr1 = [1, 2, 3];
+    $arr2 = [2, 4, 6];
+    
+    $diff = array_diff($arr1, $arr2);
+
+    print_r($diff);
+    echo "<br>";
+
+    $diff2 = array_diff($arr2, $arr1);
+
+    print_r($diff2);
+    echo "<br>";
