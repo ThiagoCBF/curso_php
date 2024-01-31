669ed56d6ab242ef7a9c0e2e3579fb7230e1e9de
Thiago Ferreira <thiagocbf@outlook.com>
2024-01-31 19:59:46 -0300

    Sort, Rsort

---
+++ 11_arrays/23_ordenando_array/index.php
@@ -0,0 +1,24 @@
+<?php
+
+    $arr = [1, 56, 2, 789, -123, 456, 0];
+
+    sort($arr);
+
+    print_r($arr);
+    echo "<br>";
+
+    $arr2 = [1, 56, 2, 789, -123, 456, 0];
+
+    rsort($arr2);
+
+    print_r($arr2);
+    echo "<br>";
+
+   $nomes = ["Thiago", "Dani", "Cris", "Bia", "Ana"];
+   
+   sort($nomes);
+
+    print_r($nomes);
+    echo "<br>";
+
+    
