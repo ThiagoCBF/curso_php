fa6f583e6db3b09030bccaf06731c47eb0fff120
Thiago Ferreira <thiagocbf@outlook.com>
2024-01-31 20:00:29 -0300

    Shuffle

---
+++ 11_arrays/27_shuffle/index.php
@@ -0,0 +1,20 @@
+<?php
+
+    $arr = range(1, 10);
+
+    shuffle($arr);
+
+    print_r($arr);
+    echo "<br>";
+
+
+    shuffle($arr);
+
+    print_r($arr);
+    echo "<br>";
+    
+    shuffle($arr);
+
+    print_r($arr);
+    echo "<br>";
+    
