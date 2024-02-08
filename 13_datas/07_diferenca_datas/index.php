8e9b3ea5dafe57e5c65a50aa6a1c5992e404dc70
Thiago Ferreira <thiagocbf@outlook.com>
2024-02-08 16:43:36 -0300

    Diff

---
+++ 13_datas/07_diferenca_datas/index.php
@@ -0,0 +1,24 @@
+<?php
+
+    $dataA = new DateTime();
+    $dataB = new DateTime();
+
+    
+    $dataB->setDate(2004, 06, 29);
+
+    print_r($dataA);
+    echo "<br>";
+    print_r($dataB);
+    echo "<br>";
+
+    $diferenca = $dataA->diff($dataB);
+
+    print_r($diferenca);
+    echo "<br>";
+
+    echo $diferenca->format("%a days");
+    echo "<br>";
+
+
+
+
