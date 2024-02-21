af642601f077513147b7f692e23a4dcabdadc273
Thiago Ferreira <thiagocbf@outlook.com>
2024-02-21 17:27:50 -0300

    Blog

--- 15_blog/post.php
+++ 15_blog/post.php
@@ -24,24 +24,24 @@
             <div class="img-container">
                 <img src="<?= $BASE_URL ?>/img/<?= $currentPost['img'] ?>" alt="<?= $currentPost['img'] ?>">
             </div>
-            <p class="post-content">Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptas, eos amet dignissimos animi suscipit, voluptates inventore ipsum in itaque aliquid quasi! Ducimus similique aspernatur dolorem deserunt delectus porro modi totam?</p>
+            <p class="post-content">Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempore laborum numquam labore ipsum quam quidem enim iure cupiditate. Modi reiciendis eligendi odit optio illo enim ipsum. Hic dolorum laborum itaque. Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptas, eos amet dignissimos animi suscipit, voluptates inventore ipsum in itaque aliquid quasi! Ducimus similique aspernatur dolorem deserunt delectus porro modi totam? Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid dignissimos, explicabo illo minima quod eveniet corrupti hic rem ab. Itaque non eum consequuntur voluptas voluptatum sint eveniet accusamus laborum molestiae.</p>
         </div>
-   </main>
-   <aside id="nav-container">
-    <h3 id="tags-title">Tags</h3>
-    <ul id="tag-list">
-        <?php foreach($currentPost['tags'] as $tag): ?>
+        <aside id="nav-container">
+            <h3 id="tags-title">Tags</h3>
+            <ul id="tag-list">
+            <?php foreach($currentPost['tags'] as $tag): ?>
             <li><a href="#"><?= $tag ?></a></li>
-        <?php endforeach; ?>
-    </ul>
-    <h3 id="categories-title">Categorias</h3>
-    <ul id="categories-list">
-    <?php foreach($categories as $category): ?>
+            <?php endforeach; ?>
+            </ul>
+            <h3 id="categories-title">Categorias</h3>
+            <ul id="categories-list">
+            <?php foreach($categories as $category): ?>
             <li><a href="#"><?= $category ?></a></li>
-        <?php endforeach; ?>
-    </ul>
+            <?php endforeach; ?>
+            </ul>
 
-   </aside>
+        </aside>
+   </main>
 
 <?php
     include_once("templates/footer.php")
